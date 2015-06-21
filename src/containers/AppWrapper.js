2d6a2f0088606d93715f9bdc37baa379c83e23b0
Chris Wheatley <chriswheatley87@gmail.com>
2015-06-21 18:41:57 +0100

    wrapper to init redux & router

---
+++ src/containers/AppWrapper.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Router, Route } from 'react-router'
+import BrowserHistory from 'react-router/lib/BrowserHistory'
+import { createRedux } from 'redux'
+import { Provider } from 'redux/react'
+import App from './App'
+import * as stores from '../stores/index'
+
+const redux = createRedux(stores)
+
+export default class AppWrapper {
+  render() {
+    return (
+      <Provider redux={redux}>
+        {() =>
+          <Router history={BrowserHistory}>
+            <Route path='/' component={App} />
+          </Router>
+        }
+      </Provider>
+    )
+  }
+}
