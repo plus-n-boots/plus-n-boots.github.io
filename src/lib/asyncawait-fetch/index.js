5117277c8a15086a71be04a060e1f46a2c2b2e09
Chris Wheatley <chriswheatley87@gmail.com>
2015-07-12 09:09:00 +0100

    simplify & allow options to be passed in

--- src/lib/asyncawait-fetch/index.js
+++ src/lib/asyncawait-fetch/index.js
@@ -1,30 +1,14 @@
 import 'fetch'
 
-async function req (request) {
-  return await fetch(request)
+async function f (request, options) {
+  return await fetch(request, options)
 }
 
-async function post (request) {
-
+async function req (request, options) {
+  const res = await f(request, options)
+  return res.json()
 }
 
-async function del (request) {
-
-}
-
-export async function get (request) {
-  const response = await req(request)
-  return response.json()
-}
-
-export async function asyncawaitFetch (request, type = 'get') {
-  if (type === 'delete') {
-    return await del(request)
-  }
-
-  if (type === 'post') {
-    return await post(request)
-  }
-
-  return await get(request)
+export async function asyncawaitFetch (request, options = null) {
+  return await req(request, options)
 }
