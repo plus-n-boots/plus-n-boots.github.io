a037c0c69b43c646db27928dc4e360dc3daa48d0
Chris Wheatley <chriswheatley87@gmail.com>
2015-07-19 11:57:16 +0100

    amend hook added const to hook amended

--- src/constants/action-types.js
+++ src/constants/action-types.js
@@ -1,4 +1,4 @@
 export const USER_LOGGED_IN = 'USER_LOGGED_IN'
 export const USER_LOGGED_OUT = 'USER_LOGGED_OUT'
-export const HOOK_ADDED = 'HOOK_ADDED'
+export const HOOK_AMENDED = 'HOOK_AMENDED'
 export const CHECK_CACHE = 'CHECK_CACHE'
