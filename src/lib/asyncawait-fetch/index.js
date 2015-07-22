eed77bea85987a5d62924d4cc3b803542256f0b4
Chris Wheatley <chriswheatley87@gmail.com>
2015-07-22 16:18:19 +0100

    comment lib

--- src/lib/asyncawait-fetch/index.js
+++ src/lib/asyncawait-fetch/index.js
@@ -1,14 +1,24 @@
 import 'fetch'
 
+/**
+ * wrapper around fetch polyfill
+ */
 async function f (request, options) {
   return await fetch(request, options)
 }
 
+/**
+ * return json by default
+ * @todo add option to return other data types
+ */
 async function req (request, options) {
   const res = await f(request, options)
   return res.json()
 }
 
+/**
+ * public api
+ */
 export async function asyncawaitFetch (request, options = null) {
   return await req(request, options)
 }
