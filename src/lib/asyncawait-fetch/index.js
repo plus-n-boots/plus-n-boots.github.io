fc44fe67a38ad428763143620d10be1671140908
Chris Wheatley <chriswheatley87@gmail.com>
2015-07-23 23:26:46 +0100

    add overrides & checking for text or json

--- src/lib/asyncawait-fetch/index.js
+++ src/lib/asyncawait-fetch/index.js
@@ -9,16 +9,37 @@ async function f (request, options) {
 
 /**
  * return json by default
- * @todo add option to return other data types
  */
-async function req (request, options) {
+async function req (request, options, type) {
   const res = await f(request, options)
+  const contentType = res.headers.get('Content-Type')
+
+  if (res.statusText === 'No Content') {
+    return null
+  }
+
+  if (type === 'html') {
+    return res.text()
+  }
+
+  if (type === 'json') {
+    return res.json()
+  }
+
+  if (contentType.indexOf('html') > -1) {
+    return res.text()
+  }
+
+  if (contentType.indexOf('json') > -1) {
+    return res.json()
+  }
+
   return res.json()
 }
 
 /**
  * public api
  */
-export async function asyncawaitFetch (request, options = null) {
+export async function asyncawaitFetch (request, options = null, type = null) {
   return await req(request, options)
 }
