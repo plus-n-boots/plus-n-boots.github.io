4f97b2b4d0e8b4c115c03677b0f219cd4a3c18b2
Chris Wheatley <chriswheatley87@gmail.com>
2015-09-12 11:19:18 +0100

    add remote instance checking & consts

--- src/constants/couchdb.js
+++ src/constants/couchdb.js
@@ -1,2 +1,4 @@
+// export const INSTANCE = 'https://couchdb-5233bd.smileupps.com/tigris-bot-1'
 export const INSTANCE = 'tigris-bot-1'
-export const REMOTE = true
+export const REMOTE = INSTANCE.includes('http') ? true : false
+export const USERNAME = 'admin'
